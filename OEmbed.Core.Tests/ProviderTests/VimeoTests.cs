161c2dce9d4105972008e9269ec6de502a88d8a3
Ingo <thewatcher@watchersnet.de>
2025-10-23 06:46:56 +0200

    removed dead link

--- OEmbed.Core.Tests/ProviderTests/VimeoTests.cs
+++ OEmbed.Core.Tests/ProviderTests/VimeoTests.cs
@@ -7,7 +7,6 @@ public class VimeoTests : IProviderTests
     [InlineData("https://vimeo.com/22439234?embedded=true&source=video_title&owner=910279")]
     [InlineData("https://vimeo.com/groups/musicvideo/videos/306797894")]
     [InlineData("https://vimeo.com/channels/music/244199526")]
-    [InlineData("https://vimeo.com/ondemand/sydneyrenaealbumrelease")]
     [InlineData("https://vimeo.com/terjes/themountain")]
     [InlineData("https://vimeo.com/terjes/themountain#t=5s")]
     public void CanEmbedTest(string url)
@@ -20,13 +19,11 @@ public class VimeoTests : IProviderTests
     }
 
 #if NET481
-
     [Theory]
     [InlineData("https://vimeo.com/22439234")]
     [InlineData("https://vimeo.com/22439234?embedded=true&source=video_title&owner=910279")]
     [InlineData("https://vimeo.com/groups/musicvideo/videos/306797894")]
     [InlineData("https://vimeo.com/channels/music/244199526")]
-    [InlineData("https://vimeo.com/ondemand/sydneyrenaealbumrelease")]
     [InlineData("https://vimeo.com/terjes/themountain")]
     [InlineData("https://vimeo.com/terjes/themountain#t=5s")]
     public void EmbedTest(string url)
@@ -45,7 +42,6 @@ public class VimeoTests : IProviderTests
     [InlineData("https://vimeo.com/22439234?embedded=true&source=video_title&owner=910279")]
     [InlineData("https://vimeo.com/groups/musicvideo/videos/306797894")]
     [InlineData("https://vimeo.com/channels/music/244199526")]
-    [InlineData("https://vimeo.com/ondemand/sydneyrenaealbumrelease")]
     [InlineData("https://vimeo.com/terjes/themountain")]
     [InlineData("https://vimeo.com/terjes/themountain#t=5s")]
     public async Task EmbedAsyncTest(string url)
